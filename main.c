af090cdac2d4890259303c7300c00d5399ff2ecd
jfakhreddine <116410841+jfakhreddine@users.noreply.github.com>
2022-11-19 18:40:18 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
   * @file    main.c
   * @author  jarray fakhreddine
   * @brief   GPIO DRIVER 
-  * @version V2.1.0
-  * @date    15-11-2022
+  * @version V2.1.2
+  * @date    19-11-2022
   *****************************************************************************/
 
 /* Includes ------------------------------------------------------------------*/
@@ -17,8 +17,14 @@ int main()
 {
  // GPIO_Clock_Enable( (uint32_t*) 0x40020401);
   GPIO_Clock_Enable(GPIOD);
-  GPIO_Config (GPIOD, PIN12, OUTPUT, OUTPUT_PUSH_PULL);
-  GPIO_Config (GPIOB,PIN10,INPUT,OUTPUT_OPEN_DRAIN);
+  GPIO_Config (GPIOD, PIN12, OUTPUT, OUTPUT_OPEN_DRAIN , LOW_SPEED);
+  GPIO_Config (GPIOD, PIN9, INPUT, OUTPUT_OPEN_DRAIN , LOW_SPEED);
+
+  GPIO_Write_Pin(GPIOD, PIN12, SET);
+  GPIO_Write_Pin(GPIOD, PIN0, RESET); 
+  
+  uint8_t ReadPinTest = (GPIO__Read_Pin(GPIOD, PIN10));
+  uint8_t ReadPinTest1 = (GPIO__Read_Pin(GPIOD, PIN9));
   while(1)
   {
   }
